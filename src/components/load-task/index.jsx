47cbbbcb5665d754f16edd062bef0b7e8e6e988b
guinnod <210103064@stu.sdu.edu.kz>
2022-10-12 15:11:08 +0600

    load task

--- src/components/load-task/index.jsx
+++ src/components/load-task/index.jsx
@@ -2,21 +2,24 @@ import './load-task.css';
 export const LoadTask = () => {
     return (
         <div className='anchor-load'>
+            <div>
             <div className='load-up'>
-                <div>Lesson:</div>
-                <input type="text" id='lesson' className='load-url'/>
+                <div className='load-text'>Lesson:</div>
+                <input type="text" id='lesson' className='load-url' />
             </div>
             <div className='load-up'>
-                <div>Тақырып:</div>
+                <div className='load-text'>Тақырып:</div>
                 <input type="text" id='title' className='load-url' />
             </div>
             <div className='load-up'>
-                <div>Бейне:</div>
-                <input type="text" id='video' className='load-url'/>
+                <div className='load-text'>Бейне:</div>
+                <input type="text" id='video' className='load-url' />
             </div>
             <div className='load-up'>
-                <div>Тапсырма:</div>
-                <input type="text" id='task' className='load-url'/>
+                <div className='load-text'>Тапсырма:</div>
+                <input type="text" id='task' className='load-url' />
+            </div>
+            
             </div>
             <div className='load-submit'>
                 <div className='load-button red-load'>
