b2e42cc80c8b4f670beb391b469a218f92a735b2
guinnod <210103064@stu.sdu.edu.kz>
2022-11-05 12:30:13 +0600

    profile-drop

--- src/components/profile-drop/index.jsx
+++ src/components/profile-drop/index.jsx
@@ -1,19 +1,19 @@
 import './profile-drop.css';
 export const ProfileDrop = () => {
     return (
-        <div className='profile-drop-anchor'>
-            <div className="drop-top">
-                <div className="ava-drop">
-                    <img src="https://i.scdn.co/image/ab6761610000e5ebdea04210d5875d455c8dedaf" alt="" className='ava-drop' />
-                    <div className="username">
+        <div className='drop__anchor'>
+            <div>
+                <div className="drop__ava">
+                    <img src="https://i.scdn.co/image/ab6761610000e5ebdea04210d5875d455c8dedaf" alt="" />
+                    <div className="drop__username">
                         <h2>Username</h2>
                         <p>Status</p>
                     </div>
                 </div>
-                <div className="drops">
-                    <h3>Zheke Cabinet</h3>
-                    <h3>Okushy retteu</h3>
-                    <h3>Okushu bagalau</h3>
+                <div className="drop__list">
+                    <h3>Жеке Кабинет</h3>
+                    <h3>Статистика</h3>
+                    <h3>Шығу</h3>
                 </div>
             </div>
         </div>
